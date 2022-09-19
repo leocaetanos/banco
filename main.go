db02459abbec0568ccb4c7116a4a93b4a7c1d706
Leonardo Santos <leocaetanos@gmail.com>
2022-09-19 19:59:13 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"banco/contas"
+	"fmt"
+)
+
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
+func main() {
+	contaDoDenis := contas.ContaPoupanca{}
+	contaDoDenis.Depositar(100)
+	PagarBoleto(&contaDoDenis, 60)
+
+	fmt.Println(contaDoDenis.ObterSaldo())
+
+	contaDaLuisa := contas.ContaCorrente{}
+	contaDaLuisa.Depositar(1000)
+	PagarBoleto(&contaDaLuisa, 400)
+
+	fmt.Println(contaDaLuisa.ObterSaldo())
+}
